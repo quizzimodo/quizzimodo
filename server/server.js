4565e4a0e8afaf8a481010ee41d351cba6bf20c1
Forrest Akin <forrest.akin@gmail.com>
2016-05-25 15:29:33 -0700

    [refactor] refactors all request handling and routing to middleware

--- server/server.js
+++ server/server.js
@@ -1,22 +1,8 @@
 var express = require('express');
+var db = require('../db_config/db_config.js');
 
 var app = express();
 
 require('./config/middleware.js')(app, express);
-
-var userController = require('./controllers/userController.js');
-var db = require('../db_config/db_config.js');
-var Users = require('../db_config/collections/users.js');
-var Quizzes = require('../db_config/collections/quizzes.js');
-var Quiz = require('../db_config/models/quiz.js');
-var QuizInvitees = require('../db_config/models/quizInvitees.js');
-var Topic = require('../db_config/models/topic.js');
-var Questions = require('../db_config/models/questions.js');
-var Answers = require('../db_config/models/answers.js');
-var UserAnswers = require('../db_config/models/userAnswers.js');
-var UserAttempts = require('../db_config/models/userAttempts.js');
-
-app.post('/api/users/signup', userController.signup);
-app.post('/api/users/signin', userController.signin);
 
 module.exports = app;
