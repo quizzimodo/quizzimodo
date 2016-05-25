cbb86fb7391891f2b1a66e542759fee3e8294a96
Forrest Akin <forrest.akin@gmail.com>
2016-05-25 15:26:19 -0700

    [add] adds error helpers and routes /api/users

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,3 +1,14 @@
+var helpers = require('./helpers.js');
+
 module.exports = function (app, express) {
+  var userRouter = express.Router();
+
   app.use(express.static(__dirname + '/../../public'));
-}
+  
+  app.use('/api/users', userRouter);
+
+  app.use(helpers.errorLogger);
+  app.use(helpers.errorHandler);
+
+  require('../routes/userRoutes.js')(userRouter);
+};
