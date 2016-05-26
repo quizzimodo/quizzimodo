357a4b852fba7af48000444dd11a2ab0c9a03d06
MKS-students <mks.students@hackreactor.com>
2016-05-25 19:21:00 -0700

    [add] adds user controller

---
+++ public/controllers/user.js
@@ -0,0 +1,19 @@
+angular.module('quizzimodo.user', [])
+
+.controller('UserController', function() {
+  $(function(){
+    $('#profiletabs ul li a').on('click', function(e){
+      e.preventDefault();
+      var newcontent = $(this).attr('href');
+
+      $('#profiletabs ul li a').removeClass('sel');
+      $(this).addClass('sel');
+
+      $('#content section').each(function(){
+        if(!$(this).hasClass('hidden')) { $(this).addClass('hidden'); }
+      });
+
+      $(newcontent).removeClass('hidden');
+  });
+})();
+})
