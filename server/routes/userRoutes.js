d54a8dc7e7d80a25a717c5f638bbbdb6a0b1c4d3
Forrest Akin <forrest.akin@gmail.com>
2016-05-25 21:37:23 -0700

    [update] adds request handlers to user route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,10 @@
 var userController = require('../controllers/userController.js');
 
 module.exports = function (app) {
+  app.get('/', userController.getUsers);
+  app.get('/:id', userController.getUser);
+  app.put('/:id', userController.updateUser);
+  app.delete('/:id', userController.deleteUser);
   app.post('/signin', userController.signin);
-  app.post('/signup', userController.signup);
+  app.post('/signup', userController.createUser);
 };
