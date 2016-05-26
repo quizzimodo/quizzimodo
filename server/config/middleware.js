e3a86425b27e93406a31e62d5cbe8217be84d96b
Forrest Akin <forrest.akin@gmail.com>
2016-05-25 21:32:50 -0700

    [update] updates middleware with quiz route

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,13 +2,16 @@ var helpers = require('./helpers.js');
 
 module.exports = function (app, express) {
   var userRouter = express.Router();
+  var quizRouter = express.Router();
 
   app.use(express.static(__dirname + '/../../public'));
   
   app.use('/api/users', userRouter);
+  app.use('/api/quizzes', quizRouter);
 
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
 
   require('../routes/userRoutes.js')(userRouter);
+  require('../routes/quizRoutes.js')(quizRouter);
 };
