f8d1a49f2df3491ab1bc749976b916be95599f02
Forrest Akin <forrest.akin@gmail.com>
2016-05-26 11:58:12 -0700

    [update] updates quiz route

--- server/routes/quizRoutes.js
+++ server/routes/quizRoutes.js
@@ -3,6 +3,7 @@ var quizController = require('../controllers/quizController.js');
 module.exports = function (app) {
   app.get('/', quizController.getQuizzes);
   app.get('/:id', quizController.getQuiz);
+  app.post('/new', quizController.createQuiz);
   app.put('/:id', quizController.updateQuiz);
   app.delete('/:id', quizController.deleteQuiz);
 };
