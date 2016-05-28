a09b49decf130394ea62457b36ba5f3229788025
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-05-28 15:47:57 -0700

    [add] adds dependent subtopic selector in select quiz view

--- public/controllers/quizzes.js
+++ public/controllers/quizzes.js
@@ -2,19 +2,37 @@ angular.module('quizzimodo.quizzes', [])
 
 .controller('QuizzesController', function($scope, $location, Quiz, $rootScope) {
   $scope.image = '../assets/avatar.png';
-  $scope.topics = ['Math', 'English', 'Spanish', 'Science'];
+  $scope.topics = [{topicName: 'Science', sub_topics: ['Physics', 'Biology', 'Chemistry', 'Astronomy']}, 
+  {topicName: 'Math', sub_topics: ['Algebra', 'Calculus', 'Trigonometry', 'Geometry']}, 
+  {topicName: 'Tech', sub_topics: ['HTML', 'CSS', 'Javascript', 'Ruby', 'Python']}, 
+  {topicName: 'Foreign Languages', sub_topics: ['Spanish', 'French', 'Italian', 'German']}, 
+  {topicName: 'English', sub_topics: ['Literature', 'Grammar', 'Spelling', 'Famous Authors']}];
 
-  $scope.quizzes = [{id: 1, name: 'Calculus Challenge', topic: 'Math'}, 
-  {id: 2, name: 'Algebra', topic: 'Math'}, 
-  {id: 3, name: 'Biology Bash', topic: 'Science'}, 
-  {id: 4, name: 'Hablo Espanol', topic: 'Spanish'}, 
-  {id: 5, name: 'Literary Classics', topic: 'English'}];
+  $scope.$watch('topicPick', function(x){
+    $scope.userTopic = $scope.topics[x]['topicName'];
+    $scope.subTopics = $scope.topics[x]['sub_topics'];
+  });
+
+  $scope.$watch('subtopicPick', function(y){
+    $scope.userSubtopic = $scope.subTopics[y];
+  });
+
+  $scope.quizzes = [{id: 1, name: 'Calculus Challenge', topic: 'Math', subtopic: 'Calculus', 
+  details: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.'}, 
+  {id: 2, name: 'Algebra', topic: 'Math', subtopic: 'Algebra', 
+  details: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.'}, 
+  {id: 3, name: 'Biology Bash', topic: 'Science', subtopic: 'Biology', 
+  details: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.'}, 
+  {id: 4, name: 'Hablo Espanol', topic: 'Foreign Languages', subtopic: 'Spanish', 
+  details: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque.'}, 
+  {id: 5, name: 'Literary Classics', topic: 'English', subtopic: 'Literature'}];
   
-  $scope.taken = 'Retake';
+  $scope.taken = 'Take';
 
-  $scope.takeQuiz = function(quiz){
-    console.log('$scope.title= ', quiz);
-    $location.path('/take_quiz');
+  $scope.takeQuiz = function(quizID){
+    console.log('quiz id equals ', quizID);
+    //$location.path('/take_quiz');
+    //Quiz.getQuiz($rootScope.user.id, quizID);
   }
 
   // $scope.startUp = function(){
