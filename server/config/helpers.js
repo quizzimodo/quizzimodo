730b44822e8a476663fb61eed1e0b3455ac7329b
Forrest Akin <forrest.akin@gmail.com>
2016-05-28 19:51:07 -0700

    [update] updates token authentication throughout app

--- server/config/helpers.js
+++ server/config/helpers.js
@@ -1,14 +1,40 @@
+var jwt  = require('jwt-simple');
+var User = require('../models/user.js');
+
 module.exports = {
   errorLogger: function (error, req, res, next) {
-    // log the error then send it to the next middleware in
-    // middleware.js
-
     console.error(error.stack);
     next(error);
   },
   errorHandler: function (error, req, res, next) {
-    // send error message to client
-    // message for gracefull error handling on app
     res.status(500).send({error: error.message});
+  },
+  decode: function (req, res, next) {
+    var token = req.headers['x-access-token'];
+
+    if (token) {
+      var decoded = jwt.decode(token, 'secret');
+      
+      if (decoded.exp <= Date.now()) {
+        res.end('Access token has expired', 400);
+      }
+
+      User.forge({id: decoded.iss})
+      .fetch({require: true})
+      .then((user) => {
+        req.user = user;
+        next();
+      })
+      .catch((err) => next(err));
+    } else {
+      next();
+    }
+  },
+  requireAuth: function(req, res, next) {
+    if (!req.user) {
+      res.end('Not authorized', 401);
+    } else {
+      next();
+    }
   }
 }
