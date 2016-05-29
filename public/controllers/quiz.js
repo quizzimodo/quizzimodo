6c321c245d326623cba9d7e7ba34b933303814f6
lstuartfry <lstuartfry@gmail.com>
2016-05-29 13:26:57 -0700

    [add] adds submitQuiz functionality on front end

--- public/controllers/quiz.js
+++ public/controllers/quiz.js
@@ -5,14 +5,20 @@ angular.module('quizzimodo.quiz', [])
   $scope.questions = [];
   $scope.question = {question: '', answers: [{}, {}, {}, {}]};
 
+  function checkFields () {
+    if($('.ng-empty').length > 0 || $('input[type=radio]:checked').length === 0) {
+      return false;
+  }
+    return true;
+  };
 
   $scope.addQuestion = function() {
-    $scope.questions.push($scope.question);
-
-    console.log("this is questions", $scope.questions, "This is a question", $scope.question);
-    $scope.question = {question: '', answers: [{}, {}, {}, {}]};
-
-
+    if(checkFields()) {
+      $scope.questions.push($scope.question);
+      $scope.question = {question: '', answers: [{}, {}, {}, {}]};
+    } else {
+      alert('Please fill out the question and answer fields, and select a correct answer');
+    }
   };
 
   $scope.editQuestion = function() {
@@ -21,5 +27,22 @@ angular.module('quizzimodo.quiz', [])
 
   $scope.saveQuestion = function() {
 
+  }
+
+  $scope.submitQuiz = function() {
+    if(checkFields()) { 
+      $scope.questions.title = $scope.quiz.quiz;
+      $scope.questions.details = $scope.quiz.details;
+      Quiz.postQuiz($scope.questions)
+      .then(function() {
+        alert('Quiz created!');
+        $location.path('/main');
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
+    } else {
+      alert('Please fill out the question and answer fields, and select a correct answer');
+    }
   }
 });
