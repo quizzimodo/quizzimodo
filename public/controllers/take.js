45184aa5a9d50e36bd41d6c2f2f4cf355246146e
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-05-29 18:23:48 -0700

    [add] adds calls to server in controller and on page error messages

--- public/controllers/take.js
+++ public/controllers/take.js
@@ -1,28 +1,46 @@
 angular.module('quizzimodo.take', [])
 
-.controller('TakeController', function($scope, $location, Quiz, $rootScope) {
-  
-  $scope.title = 'Calculus Craze';
-  $scope.questions = [{question: 'What is the derivative of sin(2x)?', answers: ['-2sin2x', '-2cos2x', '2cos2x', '2sin2x']}, 
-  {question: 'What is the integral of 2cos(2x)?', answers: ['sin2x', 'cos2x', '-cos2x', '-sin2x']}, 
-  {question: 'Which answer is the equivalent of ln(x)?', answers: ['log10(x)', 'e^x', 'log(x)', '10log(x)']}];
-  
+.controller('TakeController', function($scope, $location, Quiz) {
+
+  $scope.quiz = {
+    id: 3,
+    quiz: 'Testing',
+    details: 'A challenging quiz about calculus',
+    questions: [{question: 'What is the derivative of sin(2x)?', answers: [{id: 1, answer: 'cos2x'}, {id: 2, answer: 'sinx'}]}, 
+                {question: 'What is the integral of 2cos(2x)?', answers: [{id: 3, answer: 'sin2x'}, {id: 4, answer: 'cosx'}]}, 
+                {question: 'Which answer is the equivalent of ln(x)?', answers: [{id: 5, answer: 'e^x'}, {id: 6, answer: 'log10x'}]}]
+
+  };
+
+
+  $scope.quizID = Quiz.getData();
   $scope.answers = {};
 
-  $scope.submit = function(){
-    console.log("These are the answers", $scope.answers);
-    $location.path('/results');
-  }
-
-  console.log('This is the $scope.user', $rootScope.user);
-  // $scope.getQuiz = function(quizID){
-  //  Quiz.getQuiz($rootScope.user.id, quizID).then(function(data){
-  //     $scope.title = data.quiz;
-  //    $scope.questions = data.questions;
-  //  }).catch(function(err){
-  //    console.log(err);
-  //  });
+  // $scope.getQuiz = function(){
+  //   console.log('This is scope quiz id', $scope.quizID);
+  //   Quiz.getQuiz($scope.quizID).then(function(data){
+  //     console.log('This is data inside getQuiz on loadup: ', data);
+  //     $scope.quiz = data.data;
+  //   }).catch(function(err){
+  //       $scope.loadError = 'Error loading quiz'; 
+  //     console.log(err);
+  //   });
   // };
   // $scope.getQuiz();
+
+
+  $scope.submit = function(){
+    console.log("This is the completed quiz", $scope.quiz);
+    
+    // Quiz.postResults($scope.quiz).then(function(data){
+    //   Quiz.setData($scope.quizID);
+    //   $location.path('/results');
+    // }).catch(function(error){
+    //   $scope.submitError = 'Error submitting quiz';
+    //   console.log(error);
+    // });
+  };
+
+
 
 });
