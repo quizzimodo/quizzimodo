0ad0261a4485b3832fb52d5df28a916bce3dcf0a
Nathaniel Schwab <schwab.nathaniel@gmail.com>
2016-05-30 22:54:45 -0700

    [update] updates result controller to connect with quiz take quiz and server call

--- public/controllers/quizzes.js
+++ public/controllers/quizzes.js
@@ -1,16 +1,6 @@
 angular.module('quizzimodo.quizzes', [])
 
 .controller('QuizzesController', function($scope, $location, Quiz, $rootScope) {
-  
-  // $scope.topics = [{topicName: 'Science', sub_topics: ['Physics', 'Biology', 'Chemistry', 'Astronomy']}, 
-  // {topicName: 'Math', sub_topics: ['Algebra', 'Calculus', 'Trigonometry', 'Geometry']}, 
-  // {topicName: 'Tech', sub_topics: ['HTML', 'CSS', 'Javascript', 'Ruby', 'Python']}, 
-  // {topicName: 'Foreign Languages', sub_topics: ['Spanish', 'French', 'Italian', 'German']}, 
-  // {topicName: 'English', sub_topics: ['Literature', 'Grammar', 'Spelling', 'Famous Authors']}];
-
-
-  // $scope.topics = $rootScope.topics;
-  // console.log('This is topics: ', $scope.topics);
 
   $scope.$watch('topicPick', function(x){
     $scope.userTopic = $scope.topics[x]['topic'];
